b3a62a7a87f4ddc2f3d39afc42c8de9dda59d46f
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2023-09-19 18:13:32 +0200

    guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min;
+    this.max;
+    this.result;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.result = Math.round((this.max - this.min) / 2) + this.min;
+    return this.result;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.result;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.result;
+  }
 }
 
 module.exports = GuessingGame;
